aa124ab0113ea347198d81066ea738f61d553e1c
Glen Davies <glendaviesnz@gmail.com>
2017-07-19 21:39:50 +1200

    Add style-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
+import styled from 'styled-components';
+
 import './App.css';
 
+const Title = styled.h1`
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
+`;
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
           <div className="App-header">
-            <h2>Welcome to BookMeIn</h2>
+            <Title>Welcome to BookMeIn</Title>
           </div>
         </div>
       </MuiThemeProvider>
