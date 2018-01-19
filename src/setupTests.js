bda136395ca29a9e432e8f0c3a0a2c64430d5bc1
Glen Davies <glendaviesnz@gmail.com>
2018-01-19 22:17:01 +1300

    Set up polyfills file to stop test warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,6 @@
+import './polyfills';
 import './rxjs-operators';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-global.requestAnimationFrame = (callback) => {
-    setTimeout(callback, 0);
-};
 
 configure({ adapter: new Adapter() });
--- src/user/UserDetails.test.js
+++ src/user/UserDetails.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import UserDetails from './UserDetails';
-
 it('renders without crashing', () => {
   shallow(<UserDetails />);
 });
+
